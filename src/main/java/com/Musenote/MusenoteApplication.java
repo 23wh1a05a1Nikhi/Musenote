35a031a1ad205e0c91777a686adc76b139a94e22
Rishitha Reddy Shaganti <23wh1a05b6@bvrithyderabad.edu.in>
2025-05-15 19:32:08 +0530

    Initilazed springboot

---
+++ src/main/java/com/Musenote/MusenoteApplication.java
@@ -0,0 +1,13 @@
+package com.Musenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusenoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Musenote/MusenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusenoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
