1f818aff74c6991619e62af1b1be2a708dc7defe
Nainika-V <nainikavempalli@gmail.com>
2025-06-18 15:46:31 +0530

    fix: userid store in db along with addPost

--- src/main/java/com/controller/LyricPostController.java
+++ src/main/java/com/controller/LyricPostController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
+import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,17 +14,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dao.LyricPostDao;
+import com.dao.LyricPostRepository;
+import com.dao.UserRegDao;
+import com.dao.UserRegRepository;
 import com.model.LyricPost;
+import com.model.UserReg;
 @CrossOrigin("http://localhost:3000")
 @RestController
 public class LyricPostController {
 	@Autowired
 	LyricPostDao postDao;
 	
-	@PostMapping("addPost")
+	@Autowired
+	UserRegRepository userRepo;
+	
+	@Autowired
+	LyricPostRepository postRepo;
+	
+	@PostMapping("/addPost")
+	public ResponseEntity<?> addPost(@RequestBody LyricPost post, Principal principal) {
+	    String username = principal.getName(); // comes from token
+	    UserReg user = userRepo.findByName(username)
+	        .orElseThrow(() -> new RuntimeException("User not found"));
+
+	    post.setUserreg(user);  // <-- set the user
+	 
+
+	    postRepo.save(post);
+	    return ResponseEntity.ok("Post created successfully");
+	}
+	
+	/*@PostMapping("addPost")
 	public LyricPost addPost(@RequestBody LyricPost post) {
 		return postDao.addPost(post);
-	}
+	}*/
 	
 	
 	@GetMapping("getAllPosts")
